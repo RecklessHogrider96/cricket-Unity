95fa1102c58f603ab7ba0bb1a109acc06357eac1
Roshan-CN <roshangovind96@gmail.com>
2024-08-01 18:32:43 -0500

    Not enough vel

--- Cricket/Assets/Scripts/Game/BallController.cs
+++ Cricket/Assets/Scripts/Game/BallController.cs
@@ -1,20 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public BallCollisionDetection ballCollisionDetection; // Reference to the BallCollisionDetection script
     public Vector3 bounceTargetPosition; // The position on the pitch where the ball should bounce
+    public float bounceFactor = 0.7f; // The factor to reduce velocity after bounce
 
     private Vector3 initPosition;
-    private bool throwBall = false;
     private Vector3 currentVelocity;
+    private List<Vector3> trajectoryPoints; // List to store trajectory points for gizmo
+    private bool hasBounced = false;
 
     private void OnEnable()
     {
         initPosition = transform.position;
-
-        ResetBall();
-
         ThrowBallEvent.Instance.AddListener(OnThrowBallEventHandler);
     }
 
@@ -23,29 +23,79 @@ public class BallController : MonoBehaviour
         ThrowBallEvent.Instance.RemoveListener(OnThrowBallEventHandler);
     }
 
-    private void FixedUpdate()
-    {
-        if (throwBall)
-        {
-            ballCollisionDetection.MoveBallAndCheckForCollision(currentVelocity, bounceTargetPosition, CricketGameModel.Instance.GetBounceFactor());
-        }
-    }
-
-    private void OnThrowBallEventHandler(Vector3 bounceTargetPosition, Vector3 initialVelocity)
+    private void OnThrowBallEventHandler(Vector3 initialVelocity, Vector3 bounceTargetPosition)
     {
         ResetBall();
 
         this.bounceTargetPosition = bounceTargetPosition;
         this.currentVelocity = initialVelocity;
+        this.hasBounced = false;
 
-        throwBall = true;
+        CalculateTrajectory();
+        StartCoroutine(MoveBallAlongTrajectory());
+    }
+
+    private void CalculateTrajectory()
+    {
+        trajectoryPoints = new List<Vector3>();
+        Vector3 currentPosition = initPosition;
+        Vector3 velocity = currentVelocity;
+
+        while (currentPosition.y > 0)
+        {
+            trajectoryPoints.Add(currentPosition);
+
+            float timeStep = Time.fixedDeltaTime;
+            currentPosition += velocity * timeStep;
+            velocity += Physics.gravity * timeStep;
+
+            // Check for bounce
+            if (!hasBounced && currentPosition.z >= bounceTargetPosition.z && currentPosition.y <= bounceTargetPosition.y)
+            {
+                // Snap to the exact bounce target position
+                currentPosition = bounceTargetPosition;
+                trajectoryPoints.Add(currentPosition);
+                hasBounced = true;
+
+                // Reflect the velocity to simulate the bounce
+                velocity = Vector3.Reflect(velocity, Vector3.up) * bounceFactor;
+            }
+
+            // Break the loop if the ball has bounced and is below the target position
+            if (hasBounced && currentPosition.y < bounceTargetPosition.y && currentPosition.z > bounceTargetPosition.z)
+            {
+                break;
+            }
+        }
+
+        // Add the final position to ensure the loop ends correctly
+        trajectoryPoints.Add(currentPosition);
+    }
+
+    private IEnumerator MoveBallAlongTrajectory()
+    {
+        foreach (var point in trajectoryPoints)
+        {
+            transform.position = point;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (trajectoryPoints != null && trajectoryPoints.Count > 0)
+        {
+            Gizmos.color = Color.red;
+            for (int i = 0; i < trajectoryPoints.Count - 1; i++)
+            {
+                Gizmos.DrawLine(trajectoryPoints[i], trajectoryPoints[i + 1]);
+            }
+        }
     }
 
     private void ResetBall()
     {
         transform.position = initPosition;
-        this.currentVelocity = Vector3.zero;
-
-        throwBall = false;
+        hasBounced = false;
     }
 }
